3a4f89d4910f5d213f3449676751c28cd04ee3ca
C Jones <code@calebjones.net>
2018-12-03 20:37:06 -0500

    Query for points that are visited more than once

--- day3.sql
+++ day3.sql
@@ -13,3 +13,13 @@ SELECT CAST(SUBSTR(str, INSTR(str, '#') + 1, INSTR(str, '@') - INSTR(str, '#') -
        CAST(SUBSTR(str, INSTR(str, ':') + 2, INSTR(str, 'x') - INSTR(str, ':') - 2) AS INTEGER) as width,
        CAST(SUBSTR(str, INSTR(str, 'x') + 1) AS INTEGER) as height
 FROM strings;
+
+SELECT COUNT(*) FROM (
+WITH inches(inch) AS (SELECT 1 UNION ALL SELECT inch+1 FROM inches LIMIT 1000)
+SELECT * FROM claims
+  JOIN inches as x JOIN inches as y
+  WHERE x.inch BETWEEN claims.left AND claims.left + claims.width - 1
+    AND y.inch BETWEEN claims.top AND claims.top + claims.height - 1
+  GROUP BY x.inch, y.inch
+  HAVING COUNT(id) > 1
+);
