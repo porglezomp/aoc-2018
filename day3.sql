ffc381a5481a571194969055b5a0b134a687e62a
C Jones <code@calebjones.net>
2018-12-04 00:50:06 -0500

    Some cleanup

--- day3.sql
+++ day3.sql
@@ -1,4 +1,4 @@
-CREATE TABLE strings(str TEXT);
+CREATE TABLE strings (s TEXT);
 
 .mode line
 .import input/day3 strings
@@ -6,16 +6,18 @@ CREATE TABLE strings(str TEXT);
 
 CREATE TABLE claims (id INTEGER, left INTEGER, top INTEGER, width INTEGER, height INTEGER);
 
+-- Parse records of the form #ID @ LEFT,TOP: WIDTHxHEIGHT
 INSERT INTO claims (id, left, top, width, height)
-SELECT CAST(SUBSTR(str, INSTR(str, '#') + 1, INSTR(str, '@') - INSTR(str, '#') - 2) AS INTEGER) as id,
-       CAST(SUBSTR(str, INSTR(str, '@') + 2, INSTR(str, ',') - INSTR(str, '@') - 2) AS INTEGER) as left,
-       CAST(SUBSTR(str, INSTR(str, ',') + 1, INSTR(str, ':') - INSTR(str, ',') - 1) AS INTEGER) as top,
-       CAST(SUBSTR(str, INSTR(str, ':') + 2, INSTR(str, 'x') - INSTR(str, ':') - 2) AS INTEGER) as width,
-       CAST(SUBSTR(str, INSTR(str, 'x') + 1) AS INTEGER) as height
+SELECT CAST(SUBSTR(s, INSTR(s, '#') + 1, INSTR(s, '@') - INSTR(s, '#') - 2) AS INTEGER) as id,
+       CAST(SUBSTR(s, INSTR(s, '@') + 2, INSTR(s, ',') - INSTR(s, '@') - 2) AS INTEGER) as left,
+       CAST(SUBSTR(s, INSTR(s, ',') + 1, INSTR(s, ':') - INSTR(s, ',') - 1) AS INTEGER) as top,
+       CAST(SUBSTR(s, INSTR(s, ':') + 2, INSTR(s, 'x') - INSTR(s, ':') - 2) AS INTEGER) as width,
+       CAST(SUBSTR(s, INSTR(s, 'x') + 1) AS INTEGER) as height
 FROM strings;
 
 CREATE TABLE points (id INTEGER, x INTEGER, y INTEGER);
 
+-- All points
 WITH inches(inch) AS (SELECT 1 UNION ALL SELECT inch+1 FROM inches LIMIT 1000)
 INSERT INTO points (id, x, y)
 SELECT id, x.inch as x, y.inch as y FROM claims
@@ -23,4 +25,5 @@ SELECT id, x.inch as x, y.inch as y FROM claims
   WHERE x.inch BETWEEN claims.left AND claims.left + claims.width - 1
     AND y.inch BETWEEN claims.top AND claims.top + claims.height - 1;
 
+-- Count the points covered by more than one entry
 SELECT COUNT(*) FROM (SELECT * FROM points GROUP BY x, y HAVING COUNT(id) > 1);
