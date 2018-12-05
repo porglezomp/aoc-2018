583892582ac3a7d9250e678363f4c9a73e9c0cd9
C Jones <code@calebjones.net>
2018-12-05 02:00:30 -0500

    Remove State in favor of a proof we don't need it
    This includes an informal proof that we don't need to iterate to a
    fixed point, and that we will instead have the correct answer after
    one pass.

--- day5.hs
+++ day5.hs
@@ -1,33 +1,39 @@
-import Control.Monad.State.Strict (State, get, put, runState)
 import Data.Char (toLower, isSpace)
 import Data.Set (Set)
 import qualified Data.Set as Set
 
+{-
+Observation: you can avoid iterating this to a fixed point, since it
+will always react fully on the first pass. If you react something of
+the form abBA, then it will skip one, and react bBA, leaving A, then
+it will move back one, and reactHead on aA, completing the reaction.
+This argument can be continued by induction, since if there is further
+wrapping, the inner reaction will have finished by the time you make it
+back to the point you were at. This, we don't have to iterate to a
+fixed point, which saves some time checking if you need to repeat.
+-}
+
 canReact :: Char -> Char -> Bool
 canReact x y = toLower x == toLower y && x /= y
 
-reactHead :: String -> State Bool String
+reactHead :: String -> String
 reactHead (y : x : xs)
-  | canReact y x = put True *> pure xs
-  | otherwise = pure $ y : x : xs
-reactHead xs = pure xs
+  | canReact y x = xs
+  | otherwise = y : x : xs
+reactHead xs = xs
 
-react :: String -> State Bool String
+react :: String -> String
 react (y : x : xs)
-  | canReact y x = react xs <* put True
-  | otherwise = react (x : xs) >>= reactHead . (y :)
-react xs = pure xs
-
-fixed :: (a -> State Bool a) -> a -> a
-fixed f x =
-    let (y, changed) = runState (f x) False in
-    if changed then fixed f y else y
+  | canReact y x = react xs
+  | otherwise = reactHead (y : react (x : xs))
+react xs = xs
 
 main :: IO ()
 main = do
   polymer <- filter (not . isSpace) <$> getContents
-  print . length $ fixed react polymer
+  print . length $ react polymer
   let units = foldl (flip (Set.insert . toLower)) Set.empty polymer
-  print . minimum $ [length $ fixed react p |
+  print . minimum $ [length $ react p |
                      u <- Set.toList units,
                      let p = filter ((/= u) . toLower) polymer]
+
