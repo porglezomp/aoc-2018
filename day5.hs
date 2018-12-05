28e61b898f2b29467fe09ed6349c4d648ba69bdc
C Jones <code@calebjones.net>
2018-12-05 02:48:14 -0500

    Day 5 with zippers!

--- day5.hs
+++ day5.hs
@@ -2,38 +2,25 @@ import Data.Char (toLower, isSpace)
 import Data.Set (Set)
 import qualified Data.Set as Set
 
-{-
-Observation: you can avoid iterating this to a fixed point, since it
-will always react fully on the first pass. If you react something of
-the form abBA, then it will skip one, and react bBA, leaving A, then
-it will move back one, and reactHead on aA, completing the reaction.
-This argument can be continued by induction, since if there is further
-wrapping, the inner reaction will have finished by the time you make it
-back to the point you were at. This, we don't have to iterate to a
-fixed point, which saves some time checking if you need to repeat.
--}
+data Zipper a = Zip [a] [a]
 
-canReact :: Char -> Char -> Bool
-canReact x y = toLower x == toLower y && x /= y
+zipLength :: Zipper a -> Int
+zipLength (Zip xs ys) = length xs + length ys
 
-reactHead :: String -> String
-reactHead (y : x : xs)
-  | canReact y x = xs
-  | otherwise = y : x : xs
-reactHead xs = xs
-
-react :: String -> String
-react (y : x : xs)
-  | canReact y x = react xs
-  | otherwise = reactHead (y : react (x : xs))
-react xs = xs
+react :: Zipper Char -> Zipper Char
+react (Zip (x : xs) (y : ys))
+  | canReact y x = react (Zip xs ys)
+  | otherwise = react (Zip (y : x : xs) ys)
+    where canReact x y = toLower x == toLower y && x /= y
+react (Zip [] (y : ys)) = react (Zip [y] ys)
+react (Zip xs []) = Zip xs []
 
 main :: IO ()
 main = do
   polymer <- filter (not . isSpace) <$> getContents
-  print . length $ react polymer
+  print . zipLength . react $ Zip [] polymer
   let units = foldl (flip (Set.insert . toLower)) Set.empty polymer
-  print . minimum $ [length $ react p |
+  print . minimum $ [zipLength $ react (Zip [] p) |
                      u <- Set.toList units,
                      let p = filter ((/= u) . toLower) polymer]
 
