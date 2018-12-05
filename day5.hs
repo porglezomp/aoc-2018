b5769343ccfd443bc1aee1ca7efc548ceb4a3687
C Jones <code@calebjones.net>
2018-12-05 02:54:20 -0500

    Manual filter/react fusion

--- day5.hs
+++ day5.hs
@@ -7,20 +7,22 @@ data Zipper a = Zip [a] [a]
 zipLength :: Zipper a -> Int
 zipLength (Zip xs ys) = length xs + length ys
 
-react :: Zipper Char -> Zipper Char
-react (Zip (x : xs) (y : ys))
-  | canReact y x = react (Zip xs ys)
-  | otherwise = react (Zip (y : x : xs) ys)
+reactSkip :: Char -> Zipper Char -> Zipper Char
+reactSkip c (Zip (x : xs) (y : ys))
+  | toLower y == c = reactSkip c (Zip (x : xs) ys)
+  | canReact y x = reactSkip c (Zip xs ys)
+  | otherwise = reactSkip c (Zip (y : x : xs) ys)
     where canReact x y = toLower x == toLower y && x /= y
-react (Zip [] (y : ys)) = react (Zip [y] ys)
-react (Zip xs []) = Zip xs []
+reactSkip c (Zip [] (y : ys))
+  | toLower y == c = reactSkip c (Zip [] ys)
+  | otherwise = reactSkip c (Zip [y] ys)
+reactSkip _ (Zip xs []) = Zip xs []
 
 main :: IO ()
 main = do
   polymer <- filter (not . isSpace) <$> getContents
-  print . zipLength . react $ Zip [] polymer
   let units = foldl (flip (Set.insert . toLower)) Set.empty polymer
-  print . minimum $ [zipLength $ react (Zip [] p) |
-                     u <- Set.toList units,
-                     let p = filter ((/= u) . toLower) polymer]
+  let polymer' = Zip [] polymer
+  print . zipLength $ reactSkip ' ' polymer'
+  print . minimum $ [zipLength $ reactSkip u polymer' | u <- Set.toList units]
 
